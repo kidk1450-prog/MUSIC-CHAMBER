ab26495072e9695d888053f944b53dd6a0442e8b
kidk1450-prog <kidk1450@gmail.com>
2025-11-28 01:38:22 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-const CACHE_NAME = 'music-chamber-cache-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME='music-chamber-cache-v1';
+const urlsToCache=['/','/index.html'];
 
-self.addEventListener('install', event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-  );
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', event=>{
-  event.respondWith(
-    caches.match(event.request).then(response=>response||fetch(event.request))
-  );
+self.addEventListener('fetch',event=>{
+  event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));
 });
